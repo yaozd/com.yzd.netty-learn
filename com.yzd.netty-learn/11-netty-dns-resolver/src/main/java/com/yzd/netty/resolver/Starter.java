4b46113c9ae6661b634d6ba2eebd9790d1333eee
yaozhendong <yaozhendong@hualala.com>
2020-05-26 11:30:01 +0800

    Maven-打包非springboot程序

---
+++ com.yzd.netty-learn/11-netty-dns-resolver/src/main/java/com/yzd/netty/resolver/Starter.java
@@ -0,0 +1,11 @@
+package com.yzd.netty.resolver;
+
+/**
+ * @Author: yaozh
+ * @Description:
+ */
+public class Starter {
+    public static void main(String[] args) {
+        System.out.println("start main");
+    }
+}
