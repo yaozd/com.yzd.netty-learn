958719417f4d9d74ce48b40f3d1eaf3b338c8461
yaozhendong <yaozhendong@hualala.com>
2020-06-17 17:35:19 +0800

    update

--- com.yzd.netty-learn/12-netty-http-server-tester/src/main/java/com/yzd/http/server/tester/utils/NettyUtil.java
+++ com.yzd.netty-learn/12-netty-http-server-tester/src/main/java/com/yzd/http/server/tester/utils/NettyUtil.java
@@ -4,7 +4,6 @@ import io.netty.channel.Channel;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.ServerChannel;
 import io.netty.channel.epoll.Epoll;
-import io.netty.channel.epoll.EpollEventLoopGroup;
 import io.netty.channel.epoll.EpollServerSocketChannel;
 import io.netty.channel.epoll.EpollSocketChannel;
 import io.netty.channel.nio.NioEventLoopGroup;
@@ -18,30 +17,35 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class NettyUtil {
 
-    private NettyUtil(){}
+    private NettyUtil() {
+    }
 
-    /**private static final int WORKER_THREADS = Runtime.getRuntime().availableProcessors() * 2;*/
+    /**
+     * private static final int WORKER_THREADS = Runtime.getRuntime().availableProcessors() * 2;
+     */
     private static final int WORKER_THREADS = Runtime.getRuntime().availableProcessors();
 
     static {
         if (Epoll.isAvailable()) {
-           log.info("Using epoll");
+            log.info("Using epoll");
         } else {
             log.info("Using java.nio");
         }
     }
 
     public static EventLoopGroup newBossEventLoopGroup() {
-        if (Epoll.isAvailable()) {
-            return new EpollEventLoopGroup();
-        }
+//异常：Exception in thread "main" java.lang.IllegalStateException: incompatible event loop type: io.netty.channel.epoll.EpollEventLoop
+//        if (Epoll.isAvailable()) {
+//            return new EpollEventLoopGroup();
+//        }
         return new NioEventLoopGroup();
     }
 
     public static EventLoopGroup newWorkerEventLoopGroup() {
-        if (Epoll.isAvailable()) {
-            return new EpollEventLoopGroup(WORKER_THREADS);
-        }
+//异常：Exception in thread "main" java.lang.IllegalStateException: incompatible event loop type: io.netty.channel.epoll.EpollEventLoop
+//        if (Epoll.isAvailable()) {
+//            return new EpollEventLoopGroup(WORKER_THREADS);
+//        }
         return new NioEventLoopGroup(WORKER_THREADS);
     }
 
