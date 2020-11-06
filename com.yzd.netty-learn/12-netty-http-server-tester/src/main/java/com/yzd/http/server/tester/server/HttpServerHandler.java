54c43e2b33ecf3773ea4e64bf0d8a66e3236f2cc
yaozhendong <yaozhendong@hualala.com>
2020-11-06 16:33:10 +0800

    feat:nginx 499与如何模拟一个不完整的请求，让nginx挂起

--- com.yzd.netty-learn/12-netty-http-server-tester/src/main/java/com/yzd/http/server/tester/server/HttpServerHandler.java
+++ com.yzd.netty-learn/12-netty-http-server-tester/src/main/java/com/yzd/http/server/tester/server/HttpServerHandler.java
@@ -1,5 +1,6 @@
 package com.yzd.http.server.tester.server;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
@@ -7,9 +8,11 @@ import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.DecoderResult;
 import io.netty.handler.codec.DecoderResultProvider;
 import io.netty.handler.codec.http.*;
+import io.netty.util.CharsetUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 
+import static io.netty.buffer.Unpooled.copiedBuffer;
 import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 
 /**
@@ -23,6 +26,7 @@ public class HttpServerHandler extends SimpleChannelInboundHandler {
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, Object msg) {
+        System.out.println(ctx.channel().id());
         if (msg instanceof DecoderResultProvider) {
             DecoderResult decoderResult = ((DecoderResultProvider) msg).decoderResult();
             if (decoderResult.isFailure()) {
@@ -42,12 +46,37 @@ public class HttpServerHandler extends SimpleChannelInboundHandler {
             String uuid = request.headers().get(HEADER_NAME_UUID);
             if(StringUtils.isEmpty(uuid)){
                 responseStatus=HttpResponseStatus.FORBIDDEN;
+                //responseStatus=HttpResponseStatus.NOT_MODIFIED;
+                //responseStatus=HttpResponseStatus.NO_CONTENT;
+               // responseStatus=HttpResponseStatus.RESET_CONTENT;
+                //responseStatus=HttpResponseStatus.PARTIAL_CONTENT;
+               // responseStatus=HttpResponseStatus.MOVED_PERMANENTLY;
                 uuid="not found uuid";
                 log.warn(uuid);
             }
-            FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, responseStatus);
+            ByteBuf content = copiedBuffer("data", CharsetUtil.UTF_8);
+            FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, responseStatus,content);
             response.headers().set(HEADER_NAME_UUID, uuid);
-            ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);;
+            response.headers().set("Connection", "close");
+            //response.headers().set("Connection", "keep-alive");
+            response.headers().set("Transfer-Encoding", "chunked");
+            //response.headers().set("Content-Length", "0");
+            //response.headers().set("Content-Length", "0");
+            //ctx.writeAndFlush(response);
+            //ctx.writeAndFlush(DefaultLastHttpContent.EMPTY_LAST_CONTENT);
+            //ctx.writeAndFlush(responseOK(responseStatus,content));
+            ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
         }
+    }
+    private FullHttpResponse responseOK(HttpResponseStatus status, ByteBuf content) {
+        FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, status, content);
+        if (content != null) {
+            response.headers().set("Content-Type", "text/plain;charset=UTF-8");
+            //response.headers().set("Transfer-Encoding", "chunk");
+            //response.headers().set("Content-Length", "100");
+            //PS:Content-Length的长度必须与body的长度一致或小于才可以。（小于时则解析body不完整）
+            //response.headers().set("Content-Length", response.content().readableBytes());
+        }
+        return response;
     }
 }
